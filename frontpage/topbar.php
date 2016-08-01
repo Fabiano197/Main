3978bc59bbefe492d7d2f15537b428e696a38635
Fabiano197 <f.p.haller@bluewin.ch>
2016-08-02 01:48:36 +0200

    Umschreiben der Topbar
    Die Topbar wurde leicht umgeschrieben, um hässlichen Code zu vermeiden

--- frontpage/topbar.php
+++ frontpage/topbar.php
@@ -1,55 +1,54 @@
 <script src="js/topbar.js"></script>
+
 <div class="menu_top">
-<div id="header" class="navbar navbar-default navbar-fixed-top">
-    <div class="navbar-header">
-        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-            <i class="icon-reorder"></i>
-        </button>
-        <a id="title_name" class="navbar-brand" href="#">
-            NKSA School
-        </a>
-    </div>
-    <nav class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="index.php">Home</a>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kapitel<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Drehbuch"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Drehbuch'>"?>1. Drehbuch</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Aufloesung"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Aufloesung'>"?>2. Auflösung</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Einstellung"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Einstellung'>"?>3. Kamera: Einstellung</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "HD_und_4K"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=HD_und_4K'>"?>4. Kamera: HD und 4K</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Belichtung"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Belichtung'>"?>5. Kamera: Belichtung</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Belichtung_Teil2"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Belichtung_Teil2'>"?>6. Kamera: Belichtung Teil 2</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Licht"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Licht'>"?>7. Licht</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Ton"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Ton'>"?>8. Ton</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Klappe"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Klappe'>"?>9. Klappe</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Maske"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Maske'>"?>10. Maske</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Schauspiel"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Schauspiel'>"?>11. Schauspiel</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"];if($kapitel == "Schnitt"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Schnitt'>"?>12. Schnitt</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Postproduktion"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Postproduktion'>"?>13. Postproduktion</a></li>
-					<li><?php $tag = "<a"; if (empty($_GET)){}else{ $kapitel = $_GET["kap"]; if($kapitel == "Finanzierung"){$tag = $tag . " class='selected'";}} echo $tag . " href='kapitel.php?kap=Finanzierung'>"?>14. Finanzierung</a></li>
-				</ul>
-            </li>
-            <li>
-                <a href="downloads.php">Downloads</a>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav pull-right">
-            <li class="dropdown">
-                <a href="#" id="nbAcctDD" class="dropdown-toggle" data-toggle="dropdown">Gast <span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
-                <ul class="dropdown-menu pull-right">
-                   <li class="nohover"><a><input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1"></a></li>
-    <li class="nohover"><a><input type="text" class="form-control" placeholder="Passwort" aria-describedby="basic-addon1"></a></li>
-	<li role="separator" class="divider"></li>
-	<li><a href="profil.php">Login</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a>Register</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</div>
+	<div id="header" class="navbar navbar-default navbar-fixed-top">
+		<div class="navbar-header">
+			<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+				<i class="icon-reorder"></i>
+			</button>
+			<a id="title_name" class="navbar-brand" href="#">
+				NKSA School
+			</a>
+		</div>
+		<nav class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li>
+					<a href="index.php">Home</a>
+				</li>
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Kapitel<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<?php
+							$chapters = array("Drehbuch", "Auflösung", "Kamera: Einstellung", "Kamera: HD und 4K", "Kamera: Belichtung", "Kamera: Belichtung Teil 2", "Licht", "Ton", "Klappe", "Maske", "Schauspiel", "Schnitt", "Postproduktion", "Finanzierung");
+							for($i = 0; $i < count($chapters); $i++){
+								echo "<li>";
+								echo "<a";
+								if(!empty($_GET) && $_GET["kap"] == $chapters[$i]){
+									echo " class='selected'";
+								}
+								echo " href='kapitel.php?kap=" . $chapters[$i] . "'>" . ($i+1) . ". " . $chapters[$i] . " </a>";
+								echo "</li>";
+							}
+						?>	
+					</ul>
+				</li>
+				<li>
+					<a href="downloads.php">Downloads</a>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav pull-right">
+				<li class="dropdown">
+					<a href="#" id="nbAcctDD" class="dropdown-toggle" data-toggle="dropdown">Gast <span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
+					<ul class="dropdown-menu pull-right">
+					   <li class="nohover"><a><input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1"></a></li>
+		<li class="nohover"><a><input type="text" class="form-control" placeholder="Passwort" aria-describedby="basic-addon1"></a></li>
+		<li role="separator" class="divider"></li>
+		<li><a href="profil.php">Login</a></li>
+		<li role="separator" class="divider"></li>
+		<li><a>Register</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </div>
