2e249b2eba34a25de3cdc297f0ccee09f640b09e
Fabiano197 <f.p.haller@bluewin.ch>
2016-08-02 02:08:55 +0200

    Auslagerung Sidebar
    Die Sidebar wurde ausgelagert und der Code verschönert

---
+++ frontpage/build/sidebar.php
@@ -0,0 +1,18 @@
+﻿<div class = "sidebar">
+	<div id="sidebar-wrapper">
+		<ul class="sidebar-nav">
+			<?php
+				$chapters = array("Drehbuch", "Auflösung", "Kamera: Einstellung", "Kamera: HD und 4K", "Kamera: Belichtung", "Kamera: Belichtung Teil 2", "Licht", "Ton", "Klappe", "Maske", "Schauspiel", "Schnitt", "Postproduktion", "Finanzierung");
+				for($i = 0; $i < count($chapters); $i++){
+					echo "<li>";
+					echo "<a";
+					if(!empty($_GET) && $_GET["kap"] == $chapters[$i]){
+						echo " class='selected'";
+					}
+					echo " href='kapitel.php?kap=" . $chapters[$i] . "'>" . ($i+1) . ". " . $chapters[$i] . " </a>";
+					echo "</li>";
+				}
+			?>
+		</ul>
+	</div>
+</div>
