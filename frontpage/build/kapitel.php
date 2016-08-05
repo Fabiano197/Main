ac0fe6246962e808c24418d3e223a71be3e3cabb
Berke Ates <wppgamer@users.noreply.github.com>
2016-08-05 20:56:09 +0200

    Dynamische Auswahl
    Topbar, Sidebar und Home-Auswahl laden nun die Auwahlmöglichkeiten von
    der DB

--- frontpage/build/kapitel.php
+++ frontpage/build/kapitel.php
@@ -7,18 +7,6 @@
 					<div class="row">
 						<div class="col-lg-12">
 							<?php
-								// SQL Datenbank verlinken
-								define ( 'MYSQL_HOST',      'localhost' );
-								define ( 'MYSQL_BENUTZER',  'program' );
-								define ( 'MYSQL_KENNWORT',  'qwertzy13' );
-								define ( 'MYSQL_DATENBANK', 'sncmsdb' );
-								$db_link = mysqli_connect(MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK);
-								
-								// Falls Verlinkung zur SQL-Datenbank nicht funktioniert
-								if(!$db_link){
-									exit("Verbindungsfehler: ".mysqli_connect_error());
-								} 
-								
 								// Überpfrüfung des GET's und entsprechende Ausgabe von 404
 								if (empty($_GET) || $_GET['kap'] == ""){include "404.html"; exit();}
 								if (mysqli_fetch_array(mysqli_query($db_link,"SELECT * FROM pages Where PageID =" . $_GET["kap"])) == ""){include "404.html"; exit();}
--- frontpage/build/sidebar.php
+++ frontpage/build/sidebar.php
@@ -1,15 +1,13 @@
 ﻿<div id="sidebar-wrapper">
 	<ul class="sidebar-nav">
-		<?php
-			$chapters = array("Drehbuch", "Auflösung", "Kamera: Einstellung", "Kamera: HD und 4K", "Kamera: Belichtung", "Kamera: Belichtung Teil 2", "Licht", "Ton", "Klappe", "Maske", "Schauspiel", "Schnitt", "Postproduktion", "Finanzierung");
-			for($i = 0; $i < count($chapters); $i++){
-				echo "<li>";
-				echo "<a";
-				if(!empty($_GET) && $_GET["kap"] == ($i+1)){
+		<?php	
+			$pageinfo = mysqli_query($db_link,"SELECT `Pagename`,`PageID` FROM `pages`");
+			while ($row = mysqli_fetch_array($pageinfo)){
+				echo "<li><a";
+				if(!empty($_GET) && $_GET["kap"] == $row["PageID"]){
 					echo " class='selected'";
 				}
-				echo " href='index.php?loc=kapitel&kap=" . ($i+1) . "'>" . ($i+1) . ". " . $chapters[$i] . " </a>";
-				echo "</li>";
+				echo " href='index.php?loc=kapitel&kap=" . $row["PageID"] . "'>" . $row["PageID"] . ". " . $row["Pagename"] . " </a></li>";
 			}
 		?>
 	</ul>
