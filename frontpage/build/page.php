973ab5b5e006d9fe68543158167b44c920056130
Berke Ates <wppgamer@users.noreply.github.com>
2016-08-05 20:11:37 +0200

    Kommentar-Box gefixt

--- frontpage/build/page.php
+++ frontpage/build/page.php
@@ -1,18 +1,6 @@
 <div id="description" class="col-sm-8 post comments">
 	<div class="responsive-video">
 		<iframe src="<?php
-						// SQL Datenbank verlinken
-						define ( 'MYSQL_HOST',      'localhost' );
-						define ( 'MYSQL_BENUTZER',  'program' );
-						define ( 'MYSQL_KENNWORT',  'qwertzy13' );
-						define ( 'MYSQL_DATENBANK', 'sncmsdb' );
-						$db_link = mysqli_connect(MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK);
-								
-						// Falls Verlinkung zur SQL-Datenbank nicht funktioniert
-						if(!$db_link){
-							exit("Verbindungsfehler: ".mysqli_connect_error());
-						} 
-						
 						// Auslesen des YT-Links
 						$auslesung = mysqli_query($db_link,"SELECT * FROM pages Where PageID =" . $_GET["kap"]);
 						$ausgabe =  mysqli_fetch_array($auslesung);
