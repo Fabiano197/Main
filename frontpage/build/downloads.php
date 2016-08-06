f5c9731d00f4078c0f02c3bd5c3ae0a7c696b7c7
Berke Ates <wppgamer@users.noreply.github.com>
2016-08-06 17:56:25 +0200

    Kleiner Bugfix

--- frontpage/build/downloads.php
+++ frontpage/build/downloads.php
@@ -1,36 +1,37 @@
-<div class="container ">
+<div class="container hidden-xs">
 	<div class="jumbotron">
 		<?php 
 			// Auslesen der Titel und PDF's
-			$pageinfo = mysqli_query($db_link,"SELECT `Pagename`,`pdfs` FROM `pages`");
+			$pageinfo = mysqli_query($db_link,"SELECT `PageID`,`pdfs` FROM `pages` ORDER BY PageID");
 			$titel = array();
 			$pdf = array();
 			while ($row = mysqli_fetch_array($pageinfo)){
-				array_push($titel,$row['Pagename']);
+				array_push($titel,$row['PageID']);
 				array_push($pdf,$row['pdfs']);
 			}
 			
 			// Erstellung der Titel
-			for($i = 0;count($titel)/7 >= $i;$i++){
+			$rowsize = 7;
+			for($i = 0;count($titel)/$rowsize > $i;$i++){
 				echo '<table class="table"><thead class="thead-inverse"><tr><th>#</th>';
-				for($indti = 7*$i;$indti < $i+7 && $indti < count($titel); $indti++){
-					echo'<th>'. $titel[$indti] .'</th>';
+				for($indti = $rowsize*$i;$indti < $i*$rowsize+$rowsize && $indti < count($titel); $indti++){
+					echo'<th>Kapitel '. $titel[$indti] .'</th>';
 				}
 				echo '</tr></thead><tbody>';
 				
 				// Berechnung der Grössten pdf-serie
 				$maxsize = 0;
-				for($indti = 7*$i;$indti < $i+7 && $indti < count($titel); $indti++){
+				for($indti = $rowsize*$i;$indti < $i*$rowsize+$rowsize && $indti < count($titel); $indti++){
 					if(count(explode(",",$pdf[$indti])) > $maxsize){$maxsize = count(explode(",",$pdf[$indti]));}
 				}
 				
 				// Erstellung der PDF's
 				for($ind = 0; $ind < $maxsize; $ind++){
 					echo'<tr><th scope="row">'. ($ind + 1) .'</th>';
-						for($indti = 7*$i;$indti < $i+7; $indti++){
+						for($indti = $rowsize*$i;$indti < $i*$rowsize+$rowsize && $indti < count($titel); $indti++){
 							echo'<td>';
-							if($indti < count($titel) && count(explode(",",$pdf[$indti]))> $ind){
-								$names = explode(",",$pdf[$indti]);
+							$names = explode(",",$pdf[$indti]);
+							if(count($names)> $ind){
 								echo'<a href="/content/pdf/'. $names[$ind] .'.pdf" download>'. $names[$ind] . '.pdf</a>';
 							}
 							echo'</td>';
@@ -43,3 +44,4 @@
 		?>
 	</div>
 </div>
+
